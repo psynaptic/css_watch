3108e05558d34ca2677f0979c3b064104b197c45
Richard Burford <rich@freestylesystems.co.uk>
2011-03-23 08:03:34 -0700

    By seutje: Changed to asynchronous xhr instead of synchronous, stops browser from locking up.

--- js/css_watch.js
+++ js/css_watch.js
@@ -5,25 +5,22 @@
       var paths = Drupal.settings.cssWatch.paths;
       var poll = Drupal.settings.cssWatch.pollInterval;
       var queryString;
-
-      setInterval(function() {
-        for (i = 0; i < paths.length; i++) {
-          var path = paths[i];
-          console.log('Polling for changes...');
-          jQuery.ajax({
+      var worker = function() {
+        $.each(paths, function(i, path){
+          $.ajax({
             url: domain + path,
-            async: false, // nothing works without this
             ifModified: true,
             success: function(data, textStatus, jqXHR) {
               if (textStatus != 'notmodified') {
-                console.log('Reloading ' + domain + path);
+                console.log('Reloading ' + path);
                 queryString = '?'  + new Date().getTime();
                 $('link[href^="' + domain + path + '"]').attr("href", domain + path.replace(/\?.*|$/, queryString));
               }
             }
           });
-        }
-      }, poll);
+        });
+      };
+      setInterval(worker, poll);
     }
   };
-}(jQuery));
+})(jQuery);
